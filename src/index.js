4551ccad913fa141d9acd46e22fd8d55340f3d8f
Vilja Aspegren <aspegren.vilja@gmail.com>
2020-03-13 23:23:50 +0200

    task 4

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-console.log('Hello console!');
+/*console.log('Hello console!');
+import SodexoLunchMenu from 'sodexo-day-example';
+// Test
+console.log(SodexoLunchMenu.courses);*/
 
-const coursesEn = ["Hamburger, cream sauce and boiled potatoes",
+/* const coursesEn = ["Hamburger, cream sauce and boiled potatoes",
   "Goan style fish curry and whole grain rice",
   "Vegan Chili sin carne and whole grain rice",
   "Broccoli puree soup, side salad with two napas",
@@ -11,14 +14,24 @@ const coursesFi = ["Jauhelihapihvi, ruskeaa kermakastiketta ja keitettyä peruna
   "vegaani Chili sin carne ja täysjyväriisi",
   "Parsakeittoa,lisäkesalaatti kahdella napaksella",
   "Lunch baguette with BBQ-turkey filling",
-  "Juusto / Kana / Kasvis / Halloumi burgeri ja ranskalaiset"];
-
+  "Juusto / Kana / Kasvis / Halloumi burgeri ja ranskalaiset"]; */
+/*
 const nappi_1 = document.getElementById("kieli_en");
 const nappi_2 = document.getElementById("kieli_fi");
+let coursesEn = [];
+let coursesFi = [];
 let menu = coursesFi;
 nappi_1.style.display = "block";
 nappi_2.style.display = "none";
 display();
+
+const parseSodexoMenu = (sodexoDailyMenu) => {
+  const courses = Object.values(sodexoDailyMenu);
+  for (const course of courses) {
+    coursesFi.push(course.category + ": " +  course.title_fi);
+    coursesEn.push(course.title_en);
+  }
+};
 
 function display() {
   const list = document.querySelector('#ravintola1');
@@ -61,4 +74,95 @@ const displayRandomCourse = () => {
   alert('Maybe you shoud try: ' + pickRandom(menu));
 };
 
+*/
+
+import LunchMenu from './sodexo-day-example.json';
+
+const restaurant = document.querySelector('.menu_ravintola1');
+const finButton = document.querySelector('.finButton');
+const engButton = document.querySelector('.engButton');
+
+let reverseButton;
+let randomButton;
+
+var i, menuFi = [], menuEn = [];
+
+
+for (i in LunchMenu.courses) {
+  menuFi[i] =  LunchMenu.courses[i].title_fi;
+
+}
+
+for (i in LunchMenu.courses) {
+  menuEn[i] =  LunchMenu.courses[i].title_en;
+
+}
+
+console.log(menuFi);
+console.log(menuEn);
+
+
+
+const randomEnMeal = () => {
+  const randomItemEnglish = menuEn[Math.floor(Math.random() * menuEn.length)];
+  alert(randomItemEnglish);
+ };
+
+ const randomFiMeal = () => {
+  const randomItemFinnish = menuFi[Math.floor(Math.random() * menuFi.length)];
+  alert(randomItemFinnish);
+ };
+
+const reverseFinMenu = () => {
+  restaurant.innerHTML = menuFi.sort().reverse().join('<br><br>');
+  reverseButton = document.createElement('button');
+  restaurant.appendChild(reverseButton);
+  reverseButton.textContent = 'Palauta';
+  reverseButton.addEventListener('click', finnishMenu);
+};
+
+const reverseEnMenu = () => {
+  restaurant.innerHTML = menuEn.sort().reverse().join('<br><br>');
+  reverseButton = document.createElement('button');
+  randomButton = document.createElement('button');
+  restaurant.appendChild(reverseButton);
+  restaurant.appendChild(randomButton);
+  reverseButton.textContent = 'reset alphabetical order';
+  randomButton.textContent = 'random Meal';
+  reverseButton.addEventListener('click', englishMenu);
+  randomButton.addEventListener('click', randomEnMeal);
+};
+
+const finnishMenu = () => {
+  restaurant.innerHTML = menuFi.sort().join('<br><br>');;
+  reverseButton = document.createElement('button');
+  randomButton = document.createElement('button');
+  reverseButton.textContent = 'Järjestä';
+  randomButton.textContent = 'Satunnainen ateria';
+  restaurant.appendChild(reverseButton);
+  restaurant.appendChild(randomButton);
+  reverseButton.addEventListener('click', reverseFinMenu);
+  randomButton.addEventListener('click', randomFiMeal);
+
+};
+
+const englishMenu = () => {
+  restaurant.innerHTML = menuEn.sort().join('<br><br>');
+  reverseButton = document.createElement('button');
+  randomButton = document.createElement('button');
+  reverseButton.textContent = 'Reverse Menu';
+  randomButton.textContent = 'Random Meal';
+  restaurant.appendChild(reverseButton);
+  restaurant.appendChild(randomButton);
+  reverseButton.addEventListener('click', reverseEnMenu);
+  randomButton.addEventListener('click', randomEnMeal);
+};
+
+
+
+
+finButton.addEventListener('click', finnishMenu);
+engButton.addEventListener('click', englishMenu);
+
+finnishMenu();
 
